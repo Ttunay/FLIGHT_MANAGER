77befa9ceebd7bf0d4315bfa253738bbeab59b0d
Ttunay <tigrus232323k@gmail.com>
2024-01-10 15:02:06 +0300

    add main

--- flight_manager.c
+++ flight_manager.c
@@ -7,6 +7,9 @@
 struct Flight {
   char flightNumber[20];
   char destination;
+  int capacity;
+  int numPassengers;
+  char passengers[MAX_PASSENGER][50];
 };
 void displayFlights(const struct Flight flight[],int numFlights)
 {}
@@ -26,6 +29,59 @@ int main()
     printf("4 Exit\n");
     printf("Enter to choise: ");
     scanf("%d", &choice);
+
+    switch (choice) {
+      case 1:
+        displayFlights(flights,numFlights);
+        break;
+     case 2:
+      if(numFlights< MAX_FLIGHT){
+        printf("engter flight number: ");
+        scanf("%s", flights[numFlights].flightNumber);
+        printf("enter destination: ");
+        scanf("%s", flights[numFlights].destination);
+        printf("enter capacity: ");
+        scanf("%d", &flights[numFlights].capacity);
+        flights[numFlights].numPassengers = 0;
+
+        numFlights++;
+
+        printf("flight added seccessfully!\n");
+
+      } else {
+        printf("max number of flights reached. \n");
+      }
+     break;
+      case 3:
+      printf("enter the flight number: \n");
+      char flightNumber[20];
+      scanf("%s", flightNumber);
+
+      int flightIndex = -1;
+        for (int i =0;i<numFlights;i++) {
+        if(strcmp(flights[i].flightNumber,flightNumber)==0)
+        {
+            flightIndex = i;
+            break;
+          }
+        }
+      if(flightIndex != -1){
+          printf("enter passenger name: ");
+          char passengerName[50];
+          scanf("%s",passengerName);
+
+          addPassenger(&flights[flightIndex],passengerName);
+        } else{
+          printf("flight not flound \n");
+        }
+      break;
+
+      case 4: 
+        printf("exiting to the program.\n");
+      break;
+
+      default:
+          printf("invalid choice, please a valid option\n");
+    }
+} while (choice != 4); 
 }
-while (choice != 4);
-} 
